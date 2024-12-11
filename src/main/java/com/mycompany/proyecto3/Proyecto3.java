fb5147cb1cac582d1725d523a1d025f931107258
Raquelmc30 <rmarcar615@g.educaand.es>
2024-12-11 20:43:45 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto3/Proyecto3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto3;
+
+/**
+ *
+ * @author alumno1DAW
+ */
+public class Proyecto3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
